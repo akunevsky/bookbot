51ee6ef96d9949ed9b6cf087ef819be42e371ddb
akunevsky <andrei.kunevsky@gmail.com>
2025-08-01 15:38:01 -0400

    Added dynamic functionality and error handling

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+import sys
 from stats import word_counter, letter_counter, sorted_dict, print_formatted_list
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+filepath = sys.argv[1]
+
 def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
+    try: 
+        with open(filepath) as f:
+            file_contents = f.read()
         return(file_contents)
+    except FileNotFoundError:
+        print(f"'{filepath}' is an invalid filepath. Try books/warandpeace.txt")
+        sys.exit(2)
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text(filepath)
     return(book_text)
 
 
 book_text = main()
 print("============ BOOKBOT ============")
-print(f"Analyzing book found at books/frankenstein.txt...")
+print(f"Analyzing book found at {filepath}...")
 print("----------- Word Count ----------")
 word_counter(book_text)
 print("--------- Character Count -------")
